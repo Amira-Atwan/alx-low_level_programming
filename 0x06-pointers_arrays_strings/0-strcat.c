db36e1fe9ecee5d7488f4cf4b4cf3eabe563d255
Amira-Atwan <amiraatwan95@gmail.com>
2022-10-27 18:48:56 +0000

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,9 +18,8 @@ while (count2 >= 0)
 {
 *(dest + count) = *(src + count2);
 if (*(src + count2) == '\0')
-															break;
-count++;
-															count2++;
-																}
+										
+break;
+count++;														count2++;														}
 return (dest);
 }
